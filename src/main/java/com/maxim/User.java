e836af81c5512c7be2945c7bb3a62106a60aa80b
Toba <270317238+bl4-k@users.noreply.github.com>
2026-04-27 16:23:04 +0100

    refactor: added User class to represent GitHub user data

--- src/main/java/com/maxim/User.java
+++ src/main/java/com/maxim/User.java
@@ -1,5 +1,31 @@
 package com.maxim;
 
 public class User {
-    
+    private String name;
+    private int repos;
+    private int followers;
+    private int following;
+
+    public User(String name, int repos, int followers, int following) {
+        this.name = name;
+        this.repos = repos;
+        this.followers = followers;
+        this.following = following;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRepos() {
+        return repos;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    public int getFollowing() {
+        return following;
+    }
 }
