606f63322748ad0eca1cbc1207f8d63c81e0c71b
Toba <270317238+bl4-k@users.noreply.github.com>
2026-04-27 18:01:17 +0100

    feat: updated CLI to allow for the user to chose options

--- src/main/java/com/maxim/Main.java
+++ src/main/java/com/maxim/Main.java
@@ -1,5 +1,6 @@
 package com.maxim;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -9,10 +10,36 @@ public class Main {
         System.out.print("Enter GitHub Username: ");
         String username = sc.nextLine();
 
+        System.out.println("1. View user summary");
+        System.out.println("2. View top repositories");
+        System.out.println("3. Exit");
+        System.out.print("Select : ");
+        int option = sc.nextInt();
+
+        switch (option) {
+            case 1:
+                userSummary(username);
+                break;
+
+            case 2:
+                userRepos(username);
+                break;
+
+            default:
+                break;
+        }
+        sc.close();
+    }
+
+    public static void userSummary(String username) {
         String data = GitHubService.getUserData(username);
         User user = GitHubParser.parseUser(data);
         GitHubFormatter.printUser(user);
+    }
 
-        sc.close();
+    public static void userRepos(String username) {
+        String data = GitHubService.getRepos(username);
+        ArrayList<Repository> repos = GitHubParser.parseRepos(data);
+        GitHubFormatter.printRepos(repos);
     }
 }
