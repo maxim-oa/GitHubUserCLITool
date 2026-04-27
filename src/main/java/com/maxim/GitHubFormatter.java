45d9512b9b5c305cee1b1de0ddd8e6dfb5b6a64c
Toba <270317238+bl4-k@users.noreply.github.com>
2026-04-27 18:00:08 +0100

    feat: added printRepos method to print the repos

--- src/main/java/com/maxim/GitHubFormatter.java
+++ src/main/java/com/maxim/GitHubFormatter.java
@@ -1,5 +1,7 @@
 package com.maxim;
 
+import java.util.ArrayList;
+
 public class GitHubFormatter {
 
     public static void printUser(User user) {
@@ -8,14 +10,30 @@ public class GitHubFormatter {
         System.out.println("GitHub User Summary");
         System.err.println("================================");
 
-        System.out.println("Name    :   " + user.getName());
-        System.out.println("Repos   :   " + user.getRepos());
+        System.out.println("Name        :   " + user.getName());
+        System.out.println("Repos       :   " + user.getRepos());
         System.out.println("Followers   :   " + user.getFollowers());
         System.out.println("Following   :   " + user.getFollowing());
 
         System.err.println("================================");
 
-        
     }
-    
+
+    public static void printRepos(ArrayList<Repository> repos) {
+
+        System.err.println("\n================================");
+        System.out.println("GitHub User Repos");
+        System.err.println("================================");
+
+        for (Repository repo : repos) {
+            System.out.println("Name    :   " + repo.getName());
+            System.out.println("Stars   :   " + repo.getStars());
+            System.out.println("Forks   :   " + repo.getForks());
+            System.out.println("URL     :   " + repo.getUrl());
+
+            System.err.println("================================");
+        }
+
+    }
+
 }
